d96f6723b68c49e8c6bec3f20f9335bdbf0ad346
dkarnani <d_karnani@outlook.com>
2020-06-08 16:52:40 -0700

    updating app.js

--- react-php/src/App.js
+++ react-php/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello hELLo
+          hello hELLo plz work
         </a>
       </header>
     </div>
