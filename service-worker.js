8a6f94b43e9c1de4e44982760b3d1fa5ffca26f5
dkarnani <d_karnani@outlook.com>
2020-08-16 20:21:27 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7aa0565df814d4291114e83db8e631f3.js"
+  "/precache-manifest.54555d4782f965e245fff3ae8e6cc457.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ static/js/react-syntax-highlighter_languages_refractor_arff.0026f246.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpauth0-react-sample"]=this["webpackJsonpauth0-react-sample"]||[]).push([[11],{626:function(a,t,e){"use strict";function n(a){a.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}a.exports=n,n.displayName="arff",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.0026f246.chunk.js.map
