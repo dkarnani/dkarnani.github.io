da99532606226544039059b26d18a77a9b0ff436
dkarnani <d_karnani@outlook.com>
2020-08-16 18:50:16 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee1ee98ec6e314fe108a47152c3c34e9.js"
+  "/precache-manifest.84c8b5af37fc84bf07592882d801b5bd.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/react-syntax-highlighter_languages_refractor_hsts.551ff4f1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpauth0-react-sample"]=this["webpackJsonpauth0-react-sample"]||[]).push([[77],{687:function(a,s,e){"use strict";function t(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=t,t.displayName="hsts",t.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.551ff4f1.chunk.js.map
