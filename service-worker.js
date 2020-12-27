66dbd4fe10e6951072dd39a24ad0f879a6b94400
dkarnani <d_karnani@outlook.com>
2020-12-26 23:53:50 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e7c0902b939e509480b258fd31acaaa9.js"
+  "/precache-manifest.14676962d24db79794ba884d787dfca9.js"
 );
 
 self.addEventListener('message', (event) => {
